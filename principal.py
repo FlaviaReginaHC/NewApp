ec543a3a0c82506ee4858efbbf49c5f2ec06a34b
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-15 10:54:21 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -7,11 +7,4 @@ st.image("Panda-Vermelho.png", width=500)
 st.markdown("Bem-vindo!")
 st.write("Este é um app simples criado para acesso ao meu site.")
 
-col1 = st.columns(1)
-
-with col1:
-    st.info("📘 Aula de Informática")
-
-st.write("")
-
 st.link_button("🌐 Acessar Site", "https://sites.google.com/academico.ifpb.edu.br/flaviaregina/in%C3%ADcio")
