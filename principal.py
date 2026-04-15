540a15920103a98e02028572b75b4925949989d2
FlaviaReginaHC <flavia.regina@academico.ifpb.edu.br>
2026-04-15 10:54:59 -0300

    Change markdown to title for welcome message

--- principal.py
+++ principal.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Flávia's App", page_icon="💻", layout="wide")
 
 st.image("Panda-Vermelho.png", width=500)
 
-st.markdown("Bem-vindo!")
+st.title("Bem-vindo!")
 st.write("Este é um app simples criado para acesso ao meu site.")
 
 st.link_button("🌐 Acessar Site", "https://sites.google.com/academico.ifpb.edu.br/flaviaregina/in%C3%ADcio")
